8f78680e0448bc2fbdd25d171419a9a5a441e880
mana.tokorocchi8@gmail.com <mana.tokorocchi8@gmail.com>
2026-01-06 23:39:27 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp05
+{
+    internal class Program
+    {
+        static void Main(string[] args) //課題32-13
+        { }
+            enum Day0fWeek {
+            Sunday, //0
+            Monday, //1
+            Tuesday, //2
+            Wednesday, //3
+            Thursday, //4
+            Friday, //5
+            Saturday, //6
+            }
+        }
+    enum Season{
+        Spring = 1,
+        Summer = 2,
+        Autumn = 3,
+        Winter = 4,
+    }
+    enum Day1fWeek { }
+}
